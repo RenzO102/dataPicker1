32f021eb430979a361a67eb3bdb9fe51f2abba35
RenzO102 <stson102@mail.ru>
2023-01-23 15:00:11 +0500

    4.0

---
+++ src/main/java/com/mycompany/myapp/web/rest/Bobla.java
@@ -0,0 +1,14 @@
+package com.mycompany.myapp.web.rest;
+
+public class Bobla {
+
+    private String Date;
+    private int Yandex;
+    private int mail;
+
+    public Bobla(String Date, int Yandex, int mail) {
+        this.Date = Date;
+        this.Yandex = Yandex;
+        this.mail = mail;
+    }
+}
